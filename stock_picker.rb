26b56b568c44c2469d6e5392ddc411285649d4fd
InAweofMyTism <inaweofmytism@gmail.com>
2024-09-16 06:40:14 -0400

    add loops to iterate over stockprices

--- stock_picker.rb
+++ stock_picker.rb
@@ -1,15 +1,37 @@
 def stock_picker(stock_prices_array)
-  #compare all stocks to determine bets buy sell date
+  #compare all stocks to determine best buy sell date
+
+  #assume buy and sell on day one for zeor profit
   buy_day = 0
   sell_day = 0
   profit = 0
 
+  stock_prices_array.each_with_index do |stock_price, current_day|
+    #first loop iterates over each day in the list
+
+    #create an array of all remaining days
+    remaining_days = stock_prices_array.slice(current_day + 1, stock_prices_array.size - current_day)
+    p remaining_days
+
+    remaining_days.each_with_index do |future_price, future_day|
+      #second loop iterates over remaining days
+      potential_profit = future_price - stock_price
+
+      #if potential is greater than current max profit, update values
+      if potential_profit > profit 
+        buy_day = current_day
+        sell_day = future_day
+        profit = potential_profit
+      end
+    end
+  end
 
   return [buy_day, sell_day]
 end
 
 def get_prices(prices)
   #take an array of prices and add a user input string to it
+
   #prompt user
   puts "Enter new price: ('!' to end)"
   #take in user input
