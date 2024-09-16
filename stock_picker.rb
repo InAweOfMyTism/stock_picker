8ec46649ee36f37996619a22f76d361d32d28595
InAweofMyTism <inaweofmytism@gmail.com>
2024-09-16 06:23:34 -0400

    Add initial fucntion defs and base code
    take in user input to fill out stock price array then run
    the stock picker algorithm on it displaying results

---
+++ stock_picker.rb
@@ -0,0 +1,20 @@
+def stock_picker(stock_prices_array)
+  #compare all stocks to determine bets buy sell date
+  buy_day = 0
+  sell_day = 0
+  profit = 0
+
+
+  return [buy_day, sell_day]
+end
+
+def get_prices(prices)
+  #take an array of prices and add a user input string to it
+  puts "Enter new price: ('!' to end)"
+  
+end
+
+prices = []
+get_prices(prices)
+p prices
+puts "The best day to [buy, sell] is #{stock_picker(prices)}"
